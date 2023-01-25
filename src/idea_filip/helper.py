c6836a9b750e8bc206d3ee659601a038fd86f249
Filip Dobrosavljevic <dofilip@student.ethz.ch>
2023-01-25 13:38:32 +0100

    Added some documentation

--- src/idea_filip/helper.py
+++ src/idea_filip/helper.py
@@ -3,13 +3,43 @@ from scipy import stats
 from numpy.random import default_rng
 import numpy as np
 
+
 def generate_random_matrix(N, K):
+    """Generates a random NxK matrix with density 0.1
+
+    Parameters
+    ----------
+    N: number of matrix rows
+    K: number of matrix columns
+
+    Returns
+    -------
+    S: A sparse matrix object
+    """
     rng = default_rng()
     rvs = stats.poisson(5, loc=5).rvs
     S = random(N, K, density=0.1, random_state=rng, data_rvs=rvs)
     return S
 
 def get_grid_format(A, nnz, N, K, Nt, Kt):
+    """Returns the grid format for the input matrix
+
+    Parameters
+    ----------
+    A: input matrix with size NxK
+    nnz: number of non-zero elements in the matrix
+    N: number of matrix rows
+    K: number of matrix columns
+    Nt: number of grid rows
+    Kt: number of grid columns
+
+    Returns
+    -------
+    A_i: row indices
+    A_j: col indices
+    A_val: non-zero values
+    A_grid_ptr: pointer pointing to first element in the grid
+    """
     px = N//Nt
     py = K//Kt
 
@@ -38,6 +68,21 @@ def get_grid_format(A, nnz, N, K, Nt, Kt):
     return A_i, A_j, A_val, A_grid_ptr
 
 def build_matrix_from_grid(N, K, A_i, A_j, A_val, A_grid_ptr):
+    """Builds a matrix from the grid format
+
+    Parameters
+    ----------
+    N: number of matrix rows
+    K: number of matrix columns
+    A_i: row indices
+    A_j: col indices
+    A_val: non-zero values
+    A_grid_ptr: pointer pointing to first element in the grid
+
+    Returns
+    -------
+    A: output matrix with size NxK
+    """
     A = np.zeros((N,K))
     for i in range(len(A_val)):
         row = A_i[i]

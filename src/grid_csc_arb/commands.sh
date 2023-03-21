b1c1fce8ee901cdb761bd8d37f903bd971d3cfaf
Filip Dobrosavljevic <dofilip@student.ethz.ch>
2023-03-21 15:26:41 +0100

    Added files for 2x3 example, out of mem

--- src/grid_csc_arb/commands.sh
+++ src/grid_csc_arb/commands.sh
@@ -3,8 +3,8 @@
 set -e
 
 
-cslc ./layout.csl --fabric-dims=10,5 --fabric-offsets=4,1 --params=width:3,height:3,Nt:2,Kt:2,M:8,A_val_len:2,A_rowidx_len:2,A_colptr_len:3,LAUNCH_ID:4 -o=out --memcpy --channels=1 --width-west-buf=0 --width-east-buf=0
+cslc ./layout.csl --fabric-dims=9,4 --fabric-offsets=4,1 --params=width:2,height:2,Nt:3,Kt:3,M:8,A_val_len:3,A_rowidx_len:3,A_colptr_len:4,LAUNCH_ID:4 -o=out --memcpy --channels=1 --width-west-buf=0 --width-east-buf=0
 
 echo "Running simulator now!"
 
-cs_python run_memcpy.py --name out -N=6 -K=6 -M=8 -A_prefix="PE3x3" -width=3 -height=3
+cs_python run_memcpy.py --name out -N=6 -K=6 -M=8 -A_prefix="PE2x2_6x6" -width=2 -height=2

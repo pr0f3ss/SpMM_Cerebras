7fa85ae19b5a1bce0e5afb7a7320c9ac1d214e6e
Emir D <emirde@stud.ntnu.no>
2023-05-12 14:35:36 +0200

    Add benchmarking and bitcast code

--- src/grid_ellpack_dsr/commands_4x4.sh
+++ src/grid_ellpack_dsr/commands_4x4.sh
@@ -2,9 +2,8 @@
 
 set -e
 
-# IMPORTANT: Have to extend A_len by 1 for it to work!!!
-cslc ./layout.csl --fabric-dims=12,6 --fabric-offsets=4,1 --params=width:2,height:2,Nt:4,Kt:4,M:8,A_len:4,LAUNCH_ID:4 -o=out --memcpy --channels=1 --width-west-buf=0 --width-east-buf=0
+cslc ./layout.csl --fabric-dims=14,8 --fabric-offsets=4,1 --params=width:4,height:4,Nt:8,Kt:8,M:16,A_len:8,LAUNCH_ID:4 -o=out --memcpy --channels=1 --width-west-buf=0 --width-east-buf=0
 
 echo "Running simulator now!"
 
-cs_python run_memcpy.py --name out -N=8 -K=8 -M=8 -A_prefix="tmp" -width=2 -height=2
+cs_python run_memcpy.py --name out -N=32 -K=32 -M=16 -A_prefix="tmp" -width=4 -height=4
--- src/grid_ellpack_dsr/run_benchmark.sh
+++ src/grid_ellpack_dsr/run_benchmark.sh
@@ -13,17 +13,16 @@ i=3
 cd test_vectors
 # Generate A matrix
 ./a.out $A_height $A_width $A_density $grid_height $grid_width $i
-OUTPUT=($(python3 add_padding.py 3 | tr -d '[],')) # Evaluate output from python script as an array of numbers
+OUTPUT=($(python3 add_padding.py 3)) # Evaluate output from python script as an array of numbers
 # Extract numbers from array indicating padded length of arrays
-A_len=${OUTPUT[1]}
-idx_len=${OUTPUT[3]}
+A_len=${OUTPUT[2]}
 
 cd ..
 
 # IMPORTANT: Have to extend A_len by 1 for it to work!!!
-cslc ./layout.csl --fabric-dims=$(($grid_width + 7)),$(($grid_height + 2)) --fabric-offsets=4,1 --params=width:$grid_width,height:$grid_height,Nt:$(($A_height / $grid_height)),Kt:$(($A_width / $grid_width)),M:$M_width,A_len:$(($A_len + 1)) ,LAUNCH_ID:4 -o=out --memcpy --channels=1 --width-west-buf=0 --width-east-buf=0
+cslc ./layout.csl --fabric-dims=$(($grid_width + 7)),$(($grid_height + 2)) --fabric-offsets=4,1 --params=width:$grid_width,height:$grid_height,Nt:$(($A_height / $grid_height)),Kt:$(($A_width / $grid_width)),M:$M_width,A_len:$A_len,LAUNCH_ID:4 -o=out --memcpy --channels=1 --width-west-buf=0 --width-east-buf=0
 
 echo "Running simulator now!"
 
-cs_python run_memcpy.py --name out -N=$A_height -K=$A_width -M=$M_width -A_prefix="test" -width=$grid_width -height=$grid_height
+cs_python run_memcpy.py --name out -N=$A_height -K=$A_width -M=$M_width -A_prefix="tmp" -width=$grid_width -height=$grid_height
 
